ff082497261d62e2c9e98230089077141bbdcb41
Adrian Kerber <adrianmathiaskerber@gmail.com>
2025-05-24 11:53:22 -0400

    test

--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -10,9 +10,9 @@ const upload = multer();
 
 // Configuração do Cloudinary
 cloudinary.config({
-  cloud_name: "process.env.CLOUDINARY_CLOUD_NAME",
-  api_key: "process.env.CLOUDINARY_API_KEY",
-  api_secret: "rocess.env.CLOUDINARY_API_SECRET"
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!
 });
 
 // Rota para upload de imagem
