a6223a64a1394745aad657985212b6da40a18e95
Adrian Kerber <adrianmathiaskerber@gmail.com>
2025-05-24 11:03:21 -0400

    testes

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,22 @@ import cors from "cors";
 import dotenv from "dotenv";
 import plantaRoutes from "./routes/plantRoutes";
 import cuidadoRoutes from "./routes/careRoutes";
-import path from "path";
 import uploadRoutes from "./routes/uploadRoutes";
-
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+// Servir imagens da pasta uploads
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
 app.use("/api", plantaRoutes);
 app.use("/api", cuidadoRoutes);
 app.use("/api", uploadRoutes);
-app.use("/uploads", express.static(path.join(__dirname, "../uploads"))); // para servir imagens
-
 
 app.get("/", (req, res) => {
   res.send("API do PlantCare está funcionando 🌱");
--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -5,13 +5,11 @@ import fs from "fs";
 
 const router = express.Router();
 
-// Cria o diretório se não existir
 const uploadDir = path.join(__dirname, "../../uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
 
-// Configuração do multer
 const storage = multer.diskStorage({
   destination: function (_, __, cb) {
     cb(null, uploadDir);
@@ -29,8 +27,8 @@ router.post("/upload", upload.single("foto"), (req, res) => {
     return res.status(400).json({ error: "Nenhum arquivo enviado" });
   }
 
-  // Caminho público que será salvo no banco
-  const filePath = `/uploads/${req.file.filename}`;
+  const baseUrl = process.env.BASE_URL || "http://localhost:3333";
+  const filePath = `${baseUrl}/uploads/${req.file.filename}`;
   res.status(200).json({ imageUrl: filePath });
 });
 
